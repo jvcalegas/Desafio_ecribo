6297fd00992c9fbf6fb1f0018a2fdeb83a71eb82
João Victor do Carmo Calegário <99449778+jvcalegas@users.noreply.github.com>
2022-08-15 18:19:24 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,18 +1,37 @@
 
+//função onde tudo acontece. :)
 
 function somaValores(value) {
     
+    // Variavel que pede o valor já convertendo em inteiro.
+    
     let entreValor = parseInt(window.prompt("Digite um valor"));
+    // variavel da soma que terá valor inicial igual a 0
+    
         var soma = 0;
+    // Lista(array) onde ficará armazenados os números divisiveis por 3 ou 5.
+    
         var divisores = [];
+    // Laço de repetição que enquanto o contador i for menor que o numero digitado, roda o laço
+    
         for (var i = 1; i<entreValor; i++) {
+            
+            // Condição para puxar os números divisíveis por 3 ou 5, adicionar na lista e fazer a soma.
             if (i % 3 == 0 || i % 5 == 0) {
                 parseInt(divisores.push(i));
                 soma += i;
             }
         }
+    // Quando a função rodar, retorna a soma.
+    
     return soma;
 }
 
+// Defino uma constante resultado e igualo à função acima.
+
 const resultado = somaValores();
+
+// Para aparecer na tela o resultado da soma. :)
 document.write(resultado);
+
+// Obrigado! ;)
